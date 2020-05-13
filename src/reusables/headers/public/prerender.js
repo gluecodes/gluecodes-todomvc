8dfa8a2cad58cd7c27f7f85e4d5ab400d95998ca
w-eagle <przemyslaw.michalak91@gmail.com>
2020-05-13 09:15:23 +0100

    code review comment

--- src/reusables/headers/public/prerender.js
+++ src/reusables/headers/public/prerender.js
@@ -4,9 +4,7 @@ export default () => `
   <header class="${styles.header}">
     <h1>todos</h1>
     <input
-      attributes=${{
-        autofocus: ''
-      }}
+      autofocus
       class="${styles['new-todo']}"
       placeholder="What needs to be done?"
       />
