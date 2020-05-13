3482df717f0464746801496231b969542fda29c8
Krzysztof Czopp <chris@glue.codes>
2020-05-13 09:06:36 +0100

    toggle all

--- src/actions/commands/markAllTodosAs/index.js
+++ src/actions/commands/markAllTodosAs/index.js
@@ -1,3 +1,11 @@
-export default async () => {
+import { getTodosFromStorage, storeTodos } from '../../../reusables/functions'
 
+export default async (isCompleted) => {
+  const existingTodos = await getTodosFromStorage()
+
+  for (const todo of existingTodos) {
+    todo.isActive = !isCompleted
+  }
+
+  storeTodos(existingTodos)
 }
--- src/pages/index/layout.jsx
+++ src/pages/index/layout.jsx
@@ -5,9 +5,7 @@ export default ({ getSlot }) => (
   <div id="layout">
     <section className={styles.todoapp}>
       {getSlot({ id: 'header' })()}
-      {/* This section should be hidden by default and shown when there are todos */}
       {getSlot({ id: 'list' })()}
-      {/* This footer should hidden by default and shown when there are todos */}
       {getSlot({ id: 'bottomBar' })()}
     </section>
     {getSlot({ id: 'footer' })()}
